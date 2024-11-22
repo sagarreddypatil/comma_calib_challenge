4177abbf4f56c6885c71f511712cfc59ff6e9f5b
Sagar Patil <sagarreddypatil@gmail.com>
2024-11-21 18:17:15 -0800

    delete exploration scripts

--- dataset.py
+++
@@ -1,8 +0,0 @@
-import cv2
-
-def load_labels(path: str):
-    with open(path, "r") as f:
-        frames = f.readlines()
-
-    frames = [tuple(map(float, frame.split())) for frame in frames]
-    return frames
